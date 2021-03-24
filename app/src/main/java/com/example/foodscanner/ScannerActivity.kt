6d1f615c583c698b474a92ced21b02ed351ccc11
Abdel <abderaouf.serour@edu.itescia.fr>
2021-03-24 18:47:08 +0100

    MVVM on ScannerActivity

--- app/src/main/java/com/example/foodscanner/ScannerActivity.kt
+++ app/src/main/java/com/example/foodscanner/ScannerActivity.kt
@@ -3,11 +3,13 @@ package com.example.foodscanner
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.DecodeCallback
@@ -24,14 +26,59 @@ class ScannerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityScannerBinding
     private val model: ScannerViewModel by viewModels()
     private lateinit var codeScanner : CodeScanner
+    private lateinit var barCode: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityScannerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        model.getState().observe(this, { updateUi(it!!) })
+
         setUpPermissions()
         codeScanner()
+
+        binding.addButton.visibility = View.INVISIBLE
+        binding.errorMessageTextView.visibility = View.INVISIBLE
+
+        // Action on our simulateButton
+        binding.simulateButton.setOnClickListener{
+            simulateBarCode()
+        }
+        // Action on our addButton
+        binding.addButton.setOnClickListener {
+            model.addToDB(this.barCode)
+        }
+    }
+
+    private fun simulateBarCode() {
+
+    }
+
+    private fun updateUi(state: ScannerViewModelState) {
+        return when (state) {
+            ScannerViewModelState.Success -> {
+                binding.errorMessageTextView.visibility = View.INVISIBLE
+                binding.addButton.visibility = state.addButtonVisibility
+                Toast.makeText(this, "Scan réussie ! ${state.addButtonVisibility}", Toast.LENGTH_SHORT).show()
+                navigateToInventory()
+            }
+            is ScannerViewModelState.Failure -> {
+                Toast.makeText(this, "Scan raté ! ${state.addButtonVisibility}", Toast.LENGTH_SHORT).show()
+                binding.addButton.visibility = state.addButtonVisibility
+                binding.errorMessageTextView.visibility = View.VISIBLE
+                binding.errorMessageTextView.text = state.errorMessage
+            }
+            is ScannerViewModelState.UpdateScan -> {
+                Toast.makeText(this, "Update Scan ! ${state.addButtonVisibility}", Toast.LENGTH_SHORT).show()
+                binding.addButton.visibility = state.addButtonVisibility
+            }
+        }
+    }
+
+    private fun navigateToInventory() {
+        Log.i(TAG, ">>>>>>> navigate to list")
     }
 
     private fun codeScanner() {
@@ -49,11 +96,27 @@ class ScannerActivity : AppCompatActivity() {
             decodeCallback = DecodeCallback {
                 runOnUiThread {
                     binding.textView.text = it.text
+                    barCode = it.text
+                    model.scan(barCode)
+                    codeScanner.releaseResources()
+
+                    Log.i(TAG, "-----------------------------------------------------------------")
+                    Log.i(TAG, "--------> ${it.text}")
+                    Log.i(TAG, "--------> ${it.timestamp}")
+                    Log.i(TAG, "--------> ${it.barcodeFormat}")
+                    Log.i(TAG, "--------> ${it.numBits}")
+                    Log.i(TAG, "--------> ${it.rawBytes}")
+                    Log.i(TAG, "--------> ${it.resultMetadata}")
+                    Log.i(TAG, "--------> ${it.resultPoints}")
+                    Log.i(TAG, "-----------------------------------------------------------------")
+
                 }
             }
 
             errorCallback = ErrorCallback {
                 runOnUiThread {
+                    barCode = ""
+                    model.scan(barCode)
                     Log.e(TAG, "Erreur de l'initialisation de la caméra : ${it.message}")
                 }
             }
--- app/src/main/java/com/example/foodscanner/ScannerViewModel.kt
+++ app/src/main/java/com/example/foodscanner/ScannerViewModel.kt
@@ -1,7 +1,38 @@
 package com.example.foodscanner
 
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+
+sealed class ScannerViewModelState(open val errorMessage: String = "", open val addButtonVisibility: Int = View.VISIBLE) {
+
+    object Success : ScannerViewModelState()
+    data class UpdateScan(override val addButtonVisibility: Int) :
+        ScannerViewModelState("", addButtonVisibility)
+
+    data class Failure(override val errorMessage: String) : ScannerViewModelState(errorMessage, View.INVISIBLE)
+}
 
 class ScannerViewModel : ViewModel() {
 
+    private val state = MutableLiveData<ScannerViewModelState>()
+    fun getState(): LiveData<ScannerViewModelState> = state
+
+    fun scan(code: String) {
+        Log.e("Scanner", "*************** searchFood() -> ${searchFood(code)}")
+
+        if (searchFood(code)) {
+            state.value = ScannerViewModelState.Success
+        } else {
+            state.value = ScannerViewModelState.Failure("Aucun produit trouvé pour ce code barre !")
+        }
+    }
+
+    fun addToDB(code: String) {
+
+    }
 }
+
+fun searchFood(code: String): Boolean = code == "3123349014822"
