e6822948ff9324c58e613f750d972f3070115296
Abdel <abderaouf.serour@edu.itescia.fr>
2021-03-24 13:37:42 +0100

    dependencies added code-scanner + scanner in the code of ScannerActivity
    dependencies added for the code-scanner to the build.gradle and addition of the scanner in the code of our ScannerActivity

--- app/src/main/java/com/example/foodscanner/ScannerActivity.kt
+++ app/src/main/java/com/example/foodscanner/ScannerActivity.kt
@@ -1,23 +1,103 @@
 package com.example.foodscanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+
 import com.example.foodscanner.databinding.ActivityScannerBinding
+
+const val TAG = "ScannerActivity"
+const val CAMERA_REQUEST_CODE = 101
 
 class ScannerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityScannerBinding
     private val model: ScannerViewModel by viewModels()
+    private lateinit var codeScanner : CodeScanner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityScannerBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        //binding.scannerButton.setOnClickListener {
-        //}
+        setUpPermissions()
+        codeScanner()
+    }
+
+    private fun codeScanner() {
+        codeScanner = CodeScanner(this, binding.scannerView)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread {
+                    binding.textView.text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Log.e(TAG, "Erreur de l'initialisation de la caméra : ${it.message}")
+                }
+            }
+        }
+
+        binding.scannerView.setOnClickListener{
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setUpPermissions() {
+         val permission: Int = ContextCompat.checkSelfPermission(this,
+         android.Manifest.permission.CAMERA)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+             makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Vous avez besoin de la caméra pour utiliser cette application", Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.i(TAG, "L'utilisateur a autorisé la caméra")
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
