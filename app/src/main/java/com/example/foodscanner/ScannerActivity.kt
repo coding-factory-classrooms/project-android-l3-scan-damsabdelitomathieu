4d5d61f281b6afb8a95e465dddcdd8780f0957fb
Abdel <abderaouf.serour@edu.itescia.fr>
2021-03-25 18:07:47 +0100

    display of the scanned product name

--- app/src/main/java/com/example/foodscanner/ScannerActivity.kt
+++ app/src/main/java/com/example/foodscanner/ScannerActivity.kt
@@ -1,23 +1,28 @@
 package com.example.foodscanner
 
+import android.R
 import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.animation.*
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.budiyev.android.codescanner.AutoFocusMode
-import com.budiyev.android.codescanner.CodeScanner
-import com.budiyev.android.codescanner.DecodeCallback
-import com.budiyev.android.codescanner.ErrorCallback
-import com.budiyev.android.codescanner.ScanMode
-
+import com.budiyev.android.codescanner.*
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import com.example.foodscanner.databinding.ActivityScannerBinding
 import com.example.foodscanner.repository.Repository
+import com.squareup.picasso.Picasso
+
 
 const val TAG = "ScannerActivity"
 const val CAMERA_REQUEST_CODE = 101
@@ -25,9 +30,10 @@ const val CAMERA_REQUEST_CODE = 101
 class ScannerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityScannerBinding
+
     //private val model: ScannerViewModel by viewModels()
     private lateinit var model: ScannerViewModel
-    private lateinit var codeScanner : CodeScanner
+    private lateinit var codeScanner: CodeScanner
     private lateinit var barCode: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,13 +47,25 @@ class ScannerActivity : AppCompatActivity() {
 
         model.myResponse.observe(this, Observer { response ->
             if (response.isSuccessful) {
-                Log.i("Response", response.body()?.status.toString())
-                Log.i("Response", response.body()?.code.toString())
-                //Log.i("Response", response.body()?.product.toString())
-                Log.i("Response", response.body()?.status_verbos.toString())
-            } else {
-                Log.i("Response", "${response.toString()} : ${response.code().toString()}")
+                binding.textView.text = barCode
 
+                Log.i("Response", "status --> ${response.body()?.status.toString()}")
+                Log.i("Response", "code --> ${response.body()?.code.toString()}")
+                Log.i("Response", "product --> ${response.body()?.product.toString()}")
+                Log.i(
+                    "Response",
+                    "status_verbose --> ${response.body()?.status_verbose.toString()}"
+                )
+                Log.i("Response", "Marque --> ${response.body()?.getBrands()?.toString()}")
+                Log.i("Response", "name --> ${response.body()?.getFoodName()?.toString()}")
+                Log.i(
+                    "Response",
+                    "small image --> ${response.body()?.getSmallImageUrl()?.toString()}"
+                )
+                Log.i("Response", "image --> ${response.body()?.getImageUrl()?.toString()}")
+            } else {
+                reinitalizeTextView()
+                Log.i("Response", "${response.toString()} : ${response.code().toString()}")
             }
         })
 
@@ -58,9 +76,14 @@ class ScannerActivity : AppCompatActivity() {
 
         binding.addButton.visibility = View.INVISIBLE
         binding.errorMessageTextView.visibility = View.INVISIBLE
+        binding.imageView.visibility = View.INVISIBLE
+        binding.foodNametextView.visibility = View.INVISIBLE
+
+        binding.imageView.isVisible = false
+        binding.foodNametextView.isVisible = false
 
         // Action on our simulateButton
-        binding.simulateButton.setOnClickListener{
+        binding.simulateButton.setOnClickListener {
             simulateBarCode()
         }
         // Action on our addButton
@@ -71,7 +94,7 @@ class ScannerActivity : AppCompatActivity() {
 
     private fun simulateBarCode() {
         codeScanner.releaseResources()
-        barCode = "737628064502"
+        barCode = "3021760400012"
         binding.textView.text = barCode
         model.scan(barCode)
     }
@@ -79,22 +102,125 @@ class ScannerActivity : AppCompatActivity() {
     private fun updateUi(state: ScannerViewModelState) {
         return when (state) {
             ScannerViewModelState.Success -> {
-                binding.errorMessageTextView.visibility = View.INVISIBLE
-                binding.addButton.visibility = state.addButtonVisibility
-                Toast.makeText(this, "Produit trouvé !", Toast.LENGTH_LONG).show()
+                startSuccessAnimation(state)
+                navigateToInventory()
+            }
+            is ScannerViewModelState.SuccessWithInfo -> {
+                startFoodDetailAnimation(state)
+                startSuccessAnimation(state)
                 navigateToInventory()
             }
             is ScannerViewModelState.Failure -> {
-                Toast.makeText(this, "Produit non trouvé !", Toast.LENGTH_SHORT).show()
-                binding.addButton.visibility = state.addButtonVisibility
-                binding.errorMessageTextView.visibility = View.VISIBLE
-                binding.errorMessageTextView.text = state.errorMessage
-                reinitalizeTextView()
+                startFailureAnimation(state)
             }
             is ScannerViewModelState.UpdateScan -> {
                 binding.addButton.visibility = state.addButtonVisibility
             }
         }
+    }
+
+    private fun startFailureAnimation(state: ScannerViewModelState) {
+
+        Log.i("ANIMATION", "animation failure !")
+
+        binding.errorMessageTextView.text = state.errorMessage
+        binding.errorMessageTextView.visibility = View.VISIBLE
+
+        val fadeIn = AlphaAnimation(0f, 1f)
+        fadeIn.interpolator = DecelerateInterpolator() //add this
+        fadeIn.duration = 1000
+
+        val fadeOut = AlphaAnimation(1f, 0f)
+        fadeOut.interpolator = AccelerateInterpolator() //and this
+        fadeOut.startOffset = 4000
+        fadeOut.duration = 1000
+
+        val animation = AnimationSet(false) //change to false
+        animation.addAnimation(fadeIn)
+        animation.addAnimation(fadeOut)
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                binding.errorMessageTextView.isVisible = true
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.errorMessageTextView.visibility = View.INVISIBLE
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+
+        binding.addButton.animate()
+            .alpha(0f)
+            .setDuration(500)
+            .withEndAction {
+                binding.addButton.visibility = state.addButtonVisibility
+            }
+
+        binding.foodNametextView.animate()
+            .alpha(0f)
+            .setDuration(500)
+            .withEndAction {
+                binding.foodNametextView.isVisible = false
+                binding.errorMessageTextView.startAnimation(animation)
+            }
+
+        binding.imageView.animate()
+            .alpha(0f)
+            .setDuration(500)
+            .withEndAction {
+                binding.imageView.isVisible = false
+            }
+
+        reinitalizeTextView()
+    }
+
+    private fun startSuccessAnimation(state: ScannerViewModelState) {
+
+        Log.i("ANIMATION", "animation success !")
+
+        binding.errorMessageTextView.isVisible = false
+        binding.addButton.visibility = state.addButtonVisibility
+        binding.addButton.alpha = 0f
+
+        binding.addButton.animate()
+            .alpha(1f)
+            .setDuration(2000)
+    }
+
+    private fun startFoodDetailAnimation(state: ScannerViewModelState) {
+
+        Log.i("ANIMATION", "food detail animation !")
+
+        binding.imageView.isVisible = true
+        binding.foodNametextView.isVisible = true
+
+        binding.foodNametextView.text = state.foodName
+        binding.foodNametextView.visibility = state.foodNameVisibility
+
+        binding.imageView.visibility = state.imageViewVisibility
+
+        val url: String = state.imageViewUrl.replace("https", "http")
+
+        if (url.isNotBlank()) {
+            //Glide.with(this).load(url).into(binding.imageView);
+            //Picasso.get().load(url).into(binding.imageView)
+
+            Glide.with(this).load("http://static.openfoodfacts.org/images/products/302/176/040/0012/front_fr.70.400.jpg")
+                .into(binding.imageView)
+        }
+        Log.i("ANIMATION", "imageURL : $url")
+
+        binding.foodNametextView.alpha = 0f
+        binding.imageView.alpha = 0f
+
+        binding.foodNametextView.animate()
+            .alpha(1f)
+            .setDuration(2000)
+
+        binding.imageView.animate()
+            .alpha(1f)
+            .setDuration(2000)
     }
 
     private fun navigateToInventory() {
@@ -115,10 +241,9 @@ class ScannerActivity : AppCompatActivity() {
 
             decodeCallback = DecodeCallback {
                 runOnUiThread {
-                    barCode = it.text
-                    binding.textView.text = barCode
-                    model.scan(barCode)
                     codeScanner.releaseResources()
+                    barCode = it.text
+                    model.scan(barCode)
 
                     Log.i(TAG, "-----------------------------------------------------------------")
                     Log.i(TAG, "--------> ${it.text}")
@@ -142,7 +267,7 @@ class ScannerActivity : AppCompatActivity() {
             }
         }
 
-        binding.scannerView.setOnClickListener{
+        binding.scannerView.setOnClickListener {
             codeScanner.startPreview()
             reinitalizeTextView()
         }
@@ -163,23 +288,37 @@ class ScannerActivity : AppCompatActivity() {
     }
 
     private fun setUpPermissions() {
-         val permission: Int = ContextCompat.checkSelfPermission(this,
-         android.Manifest.permission.CAMERA)
+        val permission: Int = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.CAMERA
+        )
 
         if (permission != PackageManager.PERMISSION_GRANTED) {
-             makeRequest()
+            makeRequest()
         }
     }
 
     private fun makeRequest() {
-         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE
+        )
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         when (requestCode) {
             CAMERA_REQUEST_CODE -> {
                 if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Vous avez besoin de la caméra pour utiliser cette application", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this,
+                        "Vous avez besoin de la caméra pour utiliser cette application",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 } else {
                     Log.i(TAG, "L'utilisateur a autorisé la caméra")
                 }
--- app/src/main/java/com/example/foodscanner/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/foodscanner/api/RetrofitInstance.kt
@@ -1,26 +1,45 @@
 package com.example.foodscanner.api
 
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.CipherSuite.Companion.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256
+import okhttp3.CipherSuite.Companion.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
+import okhttp3.CipherSuite.Companion.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
 import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
+import okhttp3.TlsVersion
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
+
 
 object RetrofitInstance {
 
     private val interceptor : HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
         this.level = HttpLoggingInterceptor.Level.BODY
     }
+
+    var spec: ConnectionSpec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+        .tlsVersions(TlsVersion.TLS_1_2)
+        .cipherSuites(
+            TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+            TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+            TLS_DHE_RSA_WITH_AES_128_GCM_SHA256
+        )
+        .build()
+
+    val specs = listOf(ConnectionSpec.CLEARTEXT, ConnectionSpec.MODERN_TLS)
+
     private val client : OkHttpClient = OkHttpClient.Builder().apply {
         this.addInterceptor(interceptor)
-            .connectionSpecs(Collections.singletonList(ConnectionSpec.MODERN_TLS))
+            .connectionSpecs(specs)
+            .connectionSpecs(Collections.singletonList(spec))
     }.build()
 
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("http://world.openfoodfacts.org")
+            .baseUrl("https://world.openfoodfacts.org")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
     }
 
